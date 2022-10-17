d8ffb6294543ce24fdda1c28668091508e4532fc
bartvanvulpen <bartvanvulpen@me.com>
2022-10-17 17:26:32 +0200

    tailwind set up

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, request
-
+import time
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST','GET'])
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():  # put application's code here
-
     if request.method == 'POST':
         prompt = request.form['text']
         processed_text = prompt.lower()
@@ -13,6 +12,10 @@ def hello_world():  # put application's code here
 
     return render_template('index.html')
 
+
+@app.route('/clicked', methods=['POST'])
+def show_img():  # put application's code here
+    return "IMG HERE"
 
 if __name__ == '__main__':
     app.run()
