2026d0969bfd2a4d7101357c64fff048b370b075
bartvanvulpen <bartvanvulpen@me.com>
2022-10-17 14:20:39 +0200

    test

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,7 +6,12 @@ app = Flask(__name__)
 @app.route('/', methods=['POST','GET'])
 def hello_world():  # put application's code here
 
-    return render_template('templates/index.html')
+    if request.method == 'POST':
+        prompt = request.form['text']
+        processed_text = prompt.lower()
+        return render_template('index.html', variable=processed_text)
+
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
